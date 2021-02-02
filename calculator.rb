e325c84e2700d5aee6fef0996de37ee369d9f30c
Susana vik <susana.m.vik@gmail.com>
2021-02-02 18:51:21 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -3,5 +3,14 @@ first_number = 5
 second_number = 1
 
 sum = first_number + second_number
+  puts sum
 
-puts sum
+difference = first_number - second_number
+  puts difference
+
+product = first_number * second_number
+  puts product
+
+quotient = first_number / second_number
+  puts quotient
+  
