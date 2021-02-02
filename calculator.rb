33c53f3060f834df469257506146813f00416b7b
Susana vik <susana.m.vik@gmail.com>
2021-02-02 18:44:44 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -2,10 +2,6 @@ first_number = 5
 
 second_number = 1
 
-def sum(first_number, second_number)
-  sum = first_number + second_number
-  puts #{sum}
-  return sum
-end
+sum = first_numnber + second_number
 
-sum(first_number, second_number)
+puts sum 
