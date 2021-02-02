90f0224fd7e329dcfaefdafabbc0250414651863
Susana vik <susana.m.vik@gmail.com>
2021-02-02 18:51:46 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,15 @@
-# Add your variables here
+first_number = 5
+
+second_number = 1
+
+sum = first_number + second_number
+  puts sum
+
+difference = first_number - second_number
+  puts difference
+
+product = first_number * second_number
+  puts product
+
+quotient = first_number / second_number
+  puts quotient
